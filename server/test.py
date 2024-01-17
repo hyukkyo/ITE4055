c1fc3916f3f0c1b32d30f459ac884c1ad4ea9c30
hyukkyo <shk5644@gmail.com>
2024-01-17 22:04:13 +0900

    코드 옮기기

--- server/test.py
+++ server/test.py
@@ -20,3 +20,191 @@ matplotlib.rcParams['figure.facecolor'] = '#ffffff'
 
 # set default figure size
 matplotlib.rcParams['figure.figsize'] = (15, 7)
+
+DATA_DIR = '../input/a-large-scale-fish-dataset/Fish_Dataset/Fish_Dataset'
+
+# Get filepaths and labels
+image_dir = Path(DATA_DIR)
+filepaths = list(image_dir.glob(r'**/*.png'))
+labels = list(map(lambda x: os.path.split(os.path.split(x)[0])[1], filepaths))
+
+filepaths = pd.Series(filepaths, name='Filepath').astype(str)
+labels = pd.Series(labels, name='Label')
+
+# Concatenate filepaths and labels
+image_df = pd.concat([filepaths, labels], axis=1)
+
+# remove GT from some label names
+image_df['Label'] = image_df['Label'].apply(lambda x: x.replace(" GT", ""))
+
+# count plot for each class
+sns.countplot(x='Label', data=image_df).set(title='Count of different image classes')
+plt.show()
+
+# the images are already augumented so no need to do any transforms
+trans = transforms.Compose([transforms.Resize([128, 128]), # resize to a smaller size to avoid CUDA running out of memory
+                            transforms.ToTensor()
+                           ])
+
+images = ImageFolder(root=DATA_DIR, transform=trans)
+
+# split data to train, test
+size = len(images)
+test_size = int(0.2 * size)
+train_size = int(size - test_size)
+print(f"number of classes: {len(images.classes)}")
+print(f"total number of images: {size}")
+print(f"total number of train images: {train_size}")
+print(f"total number of test images: {test_size}")
+# random_split
+train_set, test_set = random_split(images, (train_size, test_size))
+
+# show a single image
+def show_image(img, label, dataset):
+    plt.imshow(img.permute(1, 2, 0))
+    plt.axis('off')
+    plt.title(dataset.classes[label])
+
+# show_image(*train_set[7], train_set.dataset)
+    
+show_image(*train_set[101], train_set.dataset)
+
+# create data loaders
+batch_size = 64 # larger numbers lead to CUDA running out of memory
+train_dl = DataLoader(train_set, batch_size=batch_size)
+test_dl = DataLoader(test_set, batch_size=batch_size)
+
+# visualize a batch of images
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(20, 8))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        break
+
+# convlutional block with batchnorm and max pooling
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+    
+
+# CNN with residual connections
+class FishResNet(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        self.conv1 = conv_block(in_channels, 64)
+        self.conv2 = conv_block(64, 128, pool=True)
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128))
+        
+        self.conv3 = conv_block(128, 256, pool=True)
+        self.conv4 = conv_block(256, 512, pool=True)
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4),
+                                        nn.Flatten(),
+                                        nn.Dropout(0.2),
+                                        nn.Linear(512 * 4 * 4, num_classes))
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out # add residual
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out # add residual
+        out = self.classifier(out)
+        return out
+        
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # choose device accordingly
+model = FishResNet(3, 9).to(device) # 3 color channels and 9 output classes
+criterion = nn.CrossEntropyLoss()
+optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+# model summary (helps in understanding the output shapes)
+# summary(model, (3, 128, 128))
+
+# multiclass accuracy
+def multi_acc(y_pred, y_test):
+    y_pred_softmax = torch.log_softmax(y_pred, dim = 1)
+    _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)    
+    correct_pred = (y_pred_tags == y_test).float()
+    acc = correct_pred.sum() / len(correct_pred)
+    acc = torch.round(acc * 100)
+    return acc
+
+# training loop
+epochs = 10
+losses = []
+for epoch in range(epochs):
+    # for custom progress bar
+    with tqdm(train_dl, unit="batch") as tepoch:
+        epoch_loss = 0
+        for data, target in tepoch:
+            tepoch.set_description(f"Epoch {epoch + 1}")
+            data, target = data.to(device), target.to(device) # move input to GPU
+            out = model(data)
+            loss = criterion(out, target)
+            acc = multi_acc(out, target)
+            epoch_loss += loss.item()
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
+            tepoch.set_postfix(loss = loss.item(), accuracy = acc.item()) # show loss and accuracy per batch of data
+    losses.append(epoch_loss)
+
+# plot losses
+sns.set_style("dark")
+sns.lineplot(data=losses).set(title="loss change during training", xlabel="epoch", ylabel="loss")
+# plt.show()
+
+# predict on testing data samples (the accuracy here is batch accuracy)
+y_pred_list = []
+y_true_list = []
+with torch.no_grad():
+    with tqdm(test_dl, unit="batch") as tepoch:
+        for inp, labels in tepoch:
+            inp, labels = inp.to(device), labels.to(device)
+            y_test_pred = model(inp)
+            acc = multi_acc(y_test_pred, labels)
+            _, y_pred_tag = torch.max(y_test_pred, dim = 1)
+            tepoch.set_postfix(accuracy = acc.item())
+            y_pred_list.append(y_pred_tag.cpu().numpy())
+            y_true_list.append(labels.cpu().numpy())
+
+# flatten prediction and true lists
+flat_pred = []
+flat_true = []
+for i in range(len(y_pred_list)):
+    for j in range(len(y_pred_list[i])):
+        flat_pred.append(y_pred_list[i][j])
+        flat_true.append(y_true_list[i][j])
+        
+# print(f"number of testing samples results: {len(flat_pred)}")
+        
+# calculate total testing accuracy
+print(f"Testing accuracy is: {accuracy_score(flat_true, flat_pred) * 100:.2f}%")
+
+# Display 15 random picture of the dataset with their labels
+inds = np.random.randint(len(test_set), size=15)
+fig, axes = plt.subplots(nrows=3, ncols=5, figsize=(15, 7),
+                        subplot_kw={'xticks': [], 'yticks': []})
+
+for i, ax in zip(inds, axes.flat):
+    img, label = test_set[i]
+    ax.imshow(img.permute(1, 2, 0))
+    ax.set_title(f"True: {test_set.dataset.classes[label]}\nPredicted: {test_set.dataset.classes[flat_pred[i]]}")
+plt.tight_layout()
+plt.show()
+
+# classification report
+print(classification_report(flat_true, flat_pred, target_names=images.classes))
+
+# plot confusion matrix
+idx2class = {v: k for k, v in images.class_to_idx.items()}
+confusion_matrix_df = pd.DataFrame(confusion_matrix(flat_true, flat_pred)).rename(columns=idx2class, index=idx2class)
+sns.heatmap(confusion_matrix_df, annot=True, fmt='').set(title="confusion matrix", xlabel="Predicted Label", ylabel="True Label")
+plt.show()
